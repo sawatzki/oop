0d8cef1e9bb3eb0f4551a1f673b1a7e0399e396a
unknown <artsawatzki@gmail.com>
2019-12-06 16:52:21 +0100

    bootstrap-form created

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -8,12 +9,43 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
           integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <title>OOP PHP</title>
+    <title>PHP OOP CRUD</title>
 </head>
 <body>
 
-<h1>OOP PHP</h1>
-
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 mt-5">
+            <h1 class="text-center">PHP OOP CRUD PRACT</h1>
+            <hr style="height: 1px; color: black; background-color: black;">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-5 mx-auto">
+            <form action="" method="post">
+                <div class="form-group">
+                    <label for="">Name</label>
+                    <input type="text" name="name" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="">Email</label>
+                    <input type="email" name="email" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="">Mobile No.</label>
+                    <input type="text" name="mobile" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="">Address</label>
+                    <input type="text" name="address" rows="3"  class="form-control">
+                </div>
+                <div class="form-group">
+                    <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
